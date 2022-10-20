4c4ffbcdae340da43c133b4b026612aa3bcb76c1
Aiden Kosegi <aidkose@iu.edu>
2022-10-20 16:05:56 -0400

    Home page/footer complete
    I forgot to push things, my bad. Currently working on about page.

---
+++ model/model.js
@@ -0,0 +1,11 @@
+// if (hashID != '') {
+// 	$.get(`pages/${hashID}/${hashID}.html`, function (data) {
+// 		// console.log('data ' + data);
+// 		$('#app').html(data);
+// 	});
+// } else {
+// 	$.get(`pages/home/home.html`, function (data) {
+// 		// console.log('data' + data);
+// 		$('#app').html(data);
+// 	});
+// }
